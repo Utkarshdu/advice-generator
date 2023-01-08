591a52c3a6c888d56cb90045a0aa2aa757f75034
Utkarsh Dubey <dubeyu746@gmail.com>
2023-01-08 18:37:11 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState,useEffect} from 'react' 
+import pauseMobile from "./images/pattern-divider-mobile.svg"
+import pauseDesktop from "./images/pattern-divider-desktop.svg"
+import icon from "./images/icon-dice.svg"
+
+
 
 function App() {
+  const [text,setText]=useState([])
+  const [active,setActive]=useState(false)
+
+  const handleClick=()=>{
+    setActive(current=>!current)
+  }
+
+  const fetchAdvice = async() => {
+    const res = await fetch("https://api.adviceslip.com/advice")
+    const data= await res.json()
+    console.log(data)
+    setText(data.slip)
+  } 
+  const call=()=>{
+    handleClick();
+    fetchAdvice();
+  }
+  useEffect(()=>{
+  
+    fetchAdvice()
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container"  style={{
+      backgroundColor: active ? 'salmon' : '',
+    
+      
+    }}>
+      <h1>Advice #{text.id}</h1>
+      <p style={{color : active ? 'black' : ''}}>{text.advice}</p>
+      <picture>
+        <source media="(min-width: 768px)" srcSet={pauseDesktop}/>
+        <img src={pauseMobile} />
+      </picture>
+      <div>
+        <button onClick={call}>
+          <img src={icon} alt=""/>
+        </button>
+      </div>
     </div>
   );
 }
